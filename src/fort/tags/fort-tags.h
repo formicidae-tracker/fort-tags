8399ad24fec68c5eaf9c5626465f831da8c5e066
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2025-10-30 15:15:07 +0100

    Makes it compile with -Wall -Wpedantic -Werror

--- src/fort/tags/fort-tags.h
+++ src/fort/tags/fort-tags.h
@@ -1,13 +1,25 @@
 #pragma once
 
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wpedantic"
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
 #include <apriltag/apriltag.h>
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#else
+#pragma GCC diagnostic pop
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef enum FortTagsFamily {
-	FortTags_Tag36h11=0,
+	FortTags_Tag36h11 = 0,
 	FortTags_Tag36h10,
 	FortTags_Tag36ARTag,
 	FortTags_Tag16h5,
@@ -20,23 +32,25 @@ typedef enum FortTagsFamily {
 	FortTags_Undefined
 } FortTagsFamily_e;
 
-
-typedef apriltag_family_t*(*FortTagsFamilyConstructor)();
-typedef void(*FortTagsFamilyDestructor)(apriltag_family_t*);
+typedef apriltag_family_t *(*FortTagsFamilyConstructor)();
+typedef void (*FortTagsFamilyDestructor)(apriltag_family_t *);
 
 typedef struct FortTagsFamilyInterface {
 	FortTagsFamilyConstructor Constructor;
-	FortTagsFamilyDestructor Destructor;
+	FortTagsFamilyDestructor  Destructor;
 } FortTagsFamilyInterface_t;
 
-FortTagsFamily_e FortTagsFindFamily(const char * familyName);
+FortTagsFamily_e FortTagsFindFamily(const char *familyName);
 
-const char * FortTagsGetFamilyName(FortTagsFamily_e family);
+const char *FortTagsGetFamilyName(FortTagsFamily_e family);
 
-int FortTagsGetFamily(FortTagsFamilyInterface_t * interface,FortTagsFamily_e family);
+int FortTagsGetFamily(
+    FortTagsFamilyInterface_t *interface, FortTagsFamily_e family
+);
 
-int FortTagsGetFamilyFromName(FortTagsFamilyInterface_t * interface,const char * familyName);
-
+int FortTagsGetFamilyFromName(
+    FortTagsFamilyInterface_t *interface, const char *familyName
+);
 
 #ifdef __cplusplus
 } // extern C
